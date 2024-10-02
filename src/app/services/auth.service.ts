f7eaa2eb276fa1db0a1d39eb486a3029e34f0952
k0ddd <benja.cepe123@gmail.com>
2024-10-01 21:46:31 -0300

    testeo2

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -5,19 +5,48 @@ import { Injectable } from '@angular/core';
 })
 export class AuthService {
   private isAuthenticatedUser: boolean = false;
+  private userEmail: string = '';
+  private userRole: string = '';
 
-  constructor() { }
+  constructor() { 
+    const storedEmail = localStorage.getItem('userEmail');
+    const storedRole = localStorage.getItem('userRole');
+    if (storedEmail && storedRole){
+      this.isAuthenticatedUser=true;
+      this.userEmail=storedEmail;
+      this.userRole=storedRole;
+  }
+}
 
   isAuthenticated(): boolean{
     return this.isAuthenticatedUser;
   }
+  
+  getUserEmail(): string{
+    return this.userEmail;
+  }
 
-  login(){
+  getUserRole(): string{
+    return this.userRole
+  }
+
+  login(email: string, role: string){
     this.isAuthenticatedUser = true;
+    this.userEmail = email;
+    this.userRole = role;
+
+    localStorage.setItem('userEmail', email);
+    localStorage.setItem('userRole', role);    
   }
 
   logout() {
     this.isAuthenticatedUser = false;
+    this.userEmail = '';
+    this.userRole = '';
+
+    localStorage.removeItem('userEmail');
+    localStorage.removeItem('userRole');    
+
   }
 
 }
