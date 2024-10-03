3e6c18f4a82a4301095cf098f11a92c0272ece6d
k0ddd <benja.cepe123@gmail.com>
2024-10-03 11:30:12 -0300

    Revert "añadido verificacion del login"
    This reverts commit 5694f4998cec3e373360c5bbf0fb6a3817e6fcad.

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -1,7 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { NavigationExtras, Router } from '@angular/router';
-import { ToastController } from '@ionic/angular';
-
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -9,44 +7,10 @@ import { ToastController } from '@ionic/angular';
   styleUrls: ['./login.page.scss'],
 })
 export class LoginPage implements OnInit {
-      login:any={
-      Usuario:"",
-      Password:""
-  }
-  field:string=""
 
-  constructor(private toastController: ToastController, private router: Router ) { }
+  constructor(private router: Router) { }
 
-  ngOnInit() {}
-
-  async presentToast(message: string){
-    const toast = await this.toastController.create({
-      message: message,
-      duration: 2000
-    });
-    toast.present();
-  }
-
-  ingresar(){
-    if(this.validateModel(this.login)){
-      this.presentToast("Bienvenido "+this.login.Usuario);
-      let navigationExtras:NavigationExtras={
-        state: {user:this.login.Usuario}
-      }
-      this.router.navigate(['home'], navigationExtras);
-    }else{
-      this.presentToast("falta: "+this.field);
-    }
-  }
-
-  validateModel(model:any){
-    for (var [key, value] of Object.entries(model)){
-      if (value==""){
-        this.field=key;
-        return false;
-      }
-    }
-    return true;
+  ngOnInit() {
   }
 
   goToRecuperarContrasena(){
